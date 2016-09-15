e42ce02eadce965fa1e70aa07dc6b64976abb562
liliang <649743135@qq.com>
2016-09-15 16:17:48 +0800

    学习bootstrap less 1.4.0 start

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+  less = require('gulp-less'),
+  notify = require('gulp-notify'),
+  plumber = require('gulp-plumber'),
+  webserver = require('gulp-webserver'); // 本地服务器
+
+// webserver
+gulp.task('webserver', function() {
+  gulp.src( './public/' ) // 服务器目录（./代表根目录）
+  .pipe(webserver({ // 运行gulp-webserver
+    livereload: true, // 启用LiveReload
+    open: true // 服务器启动时自动打开网页
+  }));
+});
+
+// less
+gulp.task('less', function() {
+  gulp.src(
+   [//'./less/bootstrap/bootstrap.less',
+    './less/base/base.less'//,
+    //'./less/home/home.less'
+    ])
+    //.pipe(sourcemaps.init())
+    .pipe(less())
+    //.pipe(cssmin())
+    .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+    //.pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/css'));
+});
+
+// 监听任务
+gulp.task('watch', function () {
+  gulp.watch('./less/base/*.less', ['less']);
+  gulp.watch('*.html', ['html']);
+});
+
+// 默认任务
+gulp.task('default',['less','webserver','watch']);
